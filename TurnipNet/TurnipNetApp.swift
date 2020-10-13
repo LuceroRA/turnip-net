e1d65e4058fd14ca2f6d2d0ba1ec8b60ab883a25
LuceroRA <lucero.rodrigoa@gmail.com>
2020-10-13 12:45:58 -0400

    init commit

---
+++ TurnipNet/TurnipNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurnipNetApp.swift
+//  TurnipNet
+//
+//  Created by Rodrigo Lucero on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct TurnipNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
